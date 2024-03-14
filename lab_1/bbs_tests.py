e00f96dcfc010b52662dc3d75735e723ff409365
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-03-14 22:32:35 +0100

    added long series test

--- lab_1/bbs_tests.py
+++ lab_1/bbs_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from main import generate_bbs_sequence
 
+
 class TestBBS(unittest.TestCase):
     def test_monobit(self):
         sequence = generate_bbs_sequence(20000)
@@ -9,18 +10,35 @@ class TestBBS(unittest.TestCase):
         upper_bound = 10275
         self.assertTrue(lower_bound <= num_ones <= upper_bound)
 
-    def test_poker(self):
-        #sequence = generate_bbs_sequence(20000)
+    def test_series(self):
+        # sequence = generate_bbs_sequence(20000)
         # Implementacja testu pokerowego FIPS 140-2
         pass
 
-    def test_runs(self):
-        # Implementacja testu długich serii FIPS 140-2
-        pass
+    def test_long_series(self):
+        threshold = 26
+        current_zeros = 0
+        current_ones = 0
+        sequence = generate_bbs_sequence(20000)
+        for bit in sequence:
+            if bit == 0:
+                current_zeros += 1
+                current_ones = 0
+                if current_zeros >= threshold:
+                    self.assertFalse(True, "Znaleziono długą serię zer")
+            if bit == 1:
+                current_zeros = 0
+                current_ones += 1
+                if current_ones >= threshold:
+                    self.assertFalse(True, "Znaleziono długą serię jedynek")
 
-    def test_autocorrelation(self):
+
+        self.assertTrue(True)
+
+    def test_poker(self):
         # Implementacja testu autokorelacji FIPS 140-2
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
