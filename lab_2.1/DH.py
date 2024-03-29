4b01f1ff57a05bae2e6ebc0feb507e96def60cff
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-03-29 15:03:29 +0100

    rewrote to function

--- lab_2.1/DH.py
+++ lab_2.1/DH.py
@@ -1,18 +1,19 @@
 import random
-
 import sympy
 
+def dh_keygenerator():
+    n = sympy.randprime(1000, 9999)
+    g = 1
+    while not (1 < g < n):
+        g = sympy.primitive_root(n)
 
-n = sympy.randprime(1000,9999)
-g = 1
-while not (1 < g < n):
-    g = sympy.primitive_root(n)
+    x = random.randint(1000, 9999)
+    x_pow = pow(g, x, n)
 
-x = random.randint(1000,9999)
-X = pow(g, x, n)
+    y = random.randint(1000, 9999)
+    y_pow = pow(g, y, n)
 
-y = random.randint(1000,9999)
-Y = pow(g, y, n)
+    k_a = pow(y_pow, x, n)
+    k_b = pow(x_pow, y, n)
 
-ka=pow(y, x, n)
-kb = pow(x, y, n)
+    return k_a, k_b
